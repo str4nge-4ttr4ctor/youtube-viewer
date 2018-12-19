d972507c9ce2bc5ed9df946155b8276aa339e792
kyriakos <kyriakos@poyias.com>
2018-12-19 11:17:06 +0200

    First push of the Youtube API Starter Project

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a testing starter project using the Youtube API 
+
+## Prerequisites
+
+1. Add your own personal youtube API Key in App.js ```const API_KEY = "ADD_API_KEY_HERE";```
+    you can generate one from the [Google Developer Console](https://console.developers.google.com/)
+ 
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+Install all npm dependencies
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any errors in the console.
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import _ from "lodash";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
+const API_KEY = "ADD_API_KEY_HERE";
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch("liverpool");
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+  render() {
+      const videoSearch = _.debounce(term => {
+          this.videoSearch(term);
+      }, 300);
+      return(
+          <div>
+              <SearchBar onSearchTermChange={videoSearch} />
+              <VideoDetail video={this.state.selectedVideo} />
+              <VideoList
+                  onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                  videos={this.state.videos}
+              />
+          </div>
+      )
+  }
+}
+
+export default App;
